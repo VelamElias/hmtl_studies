811083d413ef2d2febb27fc0e42c585238c2e60c
VelamElias <elnatasource@gmail.com>
2020-06-16 23:56:20 -0300

    alerta e funcoes JS

---
+++ JS/main.js
@@ -0,0 +1,38 @@
+
+function quadrado(n)
+{
+    return n*n;
+}
+
+function validaIdade(idade)
+{   
+    var validar;
+    if(idade >= 18)
+    {
+        validar = true;
+    }
+    else{
+        validar = false;
+    }
+    
+    return validar;
+}
+
+//alert(quadrado(15));
+//alert("primeiro JS");
+
+//var idade = prompt("Digite sua idade");
+/*
+if(idade < 18)
+{
+    alert("menor de idade!");
+}else{
+    alert("maior de idade!");
+}; 
+*/
+
+/*
+var data = new Date();
+alert(data.getDay());
+*/
+
