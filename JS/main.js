11a7015a048ac183947364abf83c4136655fd534
VelamElias <elnatasource@gmail.com>
2020-06-17 01:30:48 -0300

    funcao de conversao de entrada para idioma ingles

--- JS/main.js
+++ JS/main.js
@@ -1,38 +1,68 @@
-
-function quadrado(n)
+function clicou()
 {
-    return n*n;
+    alert("Botão clicado!");
 }
 
-function validaIdade(idade)
-{   
-    var validar;
-    if(idade >= 18)
-    {
-        validar = true;
-    }
-    else{
-        validar = false;
-    }
-    
-    return validar;
+//retorna uma referencia para o primeiro objeto com o id
+//innerHTML injeta um html, aqui: Obrigado por clicar 
+function agradece()
+{
+    document.getElementById("agradecimento").innerHTML = "<b><i>click para redirecionara para Digital Innovation One<i></b>";
 }
 
-//alert(quadrado(15));
-//alert("primeiro JS");
-
-//var idade = prompt("Digite sua idade");
-/*
-if(idade < 18)
+//redireciona para uma nova aba com a página especificada
+function redirect()
 {
-    alert("menor de idade!");
-}else{
-    alert("maior de idade!");
-}; 
-*/
+    window.open("https://digitalinnovation.one/");
+}
 
-/*
-var data = new Date();
-alert(data.getDay());
-*/
+//rediciona na mesma aba
+function localRedirect()
+{
+    window.location.href = "https://digitalinnovation.one/";
+}
+
+//Modifica o texto ao passar o mouse
+//elemento -> indica o elemento que está sendo passado, no nosso exemplo pode ser qualquer 
+//um dos dois id mouseMove
+function passaMouse(elemento)
+{
+    //document.getElementById("mouseMove").innerHTML = "Você passou o mouse";
+    elemento.innerHTML = "Você passou o mouse";
+}
+
+//Volta ao texto original quando o mouse sai do texto
+function voltar(elemento)
+{
+    //document.getElementById("mouseMove").innerHTML = "Passe o mouse";
+    elemento.innerHTML = "Passe o mouse";
+}
+
+//Mostra no console o valor selecionado
+//modifica o valor do paragrafo seguinte
+
+function mudaFuncao(elemento)
+{
+    console.log(elemento.value);
+    printElemento(elemento.value);
+}
+
+//modifica o valor a ser apresentado no paragrafo anterior.
+function printElemento(value)
+{
+    val = value;
+    if(value == 1)document.getElementById("valor").innerHTML = "<b>Selecionado<b> " + value;
+    else if(value == 2) document.getElementById("valor").innerHTML = "<b>Selecionado<b> " + value;
+    else document.getElementById("valor").innerHTML = "<b>Selecionado<b> " + value;
+
+    Idioma(value);
+}
+
+//Converte a seleção para inglês
+function Idioma(value)
+{
+    if(value == 1) document.getElementById("valor").innerHTML = "Number One";
+    if(value == 2) document.getElementById("valor").innerHTML = "Number Two";
+    if(value == 3) document.getElementById("valor").innerHTML = "Number Three";
+}
 
